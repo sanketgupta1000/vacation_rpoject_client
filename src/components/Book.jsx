46f150e2e9966c29705fff89cd5b0fc1b895ae65
Harshpalsinh <zalaharshpalsinh511@gmail.com>
2024-08-11 18:09:12 +0530

    Updated book info page with UI design

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,127 +1,132 @@
-import React, { useEffect } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { setLoading, setInfo, setSingleBook } from '../slices'
-import { bookService } from '../services'
-import {BookCopyRow} from "./"
+import React, { useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading, setInfo, setSingleBook } from "../slices";
+import { bookService } from "../services";
+import { BookCopyRow, UserAvatar, BackGround } from "./";
 
-function Book()
-{
+function Book() {
+  const dispatch = useDispatch();
 
-    // dispatch
-    const dispatch = useDispatch()
+  const navigate = useNavigate();
 
-    // navigate
-    const navigate = useNavigate()
+  const { bookId } = useParams();
 
-    // bookId from the url
-    const {bookId} = useParams()
+  const jwt = useSelector((state) => state.auth.token);
 
-    // token
-    const jwt = useSelector(state=>state.auth.token)
+  // using singleBook from the store
+  const bookData = useSelector((state) => state.book.singleBook);
 
-    // using singleBook from the store
-    const singleBook = useSelector(state=>state.book.singleBook)
+  // function to fetch data
+  async function fetchBook() {
+    dispatch(setLoading({ isLoading: true, loadingMsg: "Loading book..." }));
 
-    // function to fetch data
-    async function fetchBook()
-    {
-        const response = await bookService.getBookCopies(jwt, bookId)
+    try {
+      const response = await bookService.getBookCopies(jwt, bookId);
 
-        // custom status exceptions
-        if(!response.ok)
-        {
-            throw new Error(await response.json())
-        }
+      if (!response.ok) {
+        throw new Error((await response.json()).message);
+      }
 
-        // set data
-        const data = await response.json()
+      const data = await response.json();
 
-        dispatch(setSingleBook(data))
+      dispatch(setSingleBook(data));
+    } catch (error) {
+      dispatch(
+        setInfo({ shouldShow: true, infoMsg: error.message, infoType: "error" })
+      );
+      navigate("/");
+    } finally {
+      dispatch(setLoading({ isLoading: false, loadingMsg: "" }));
     }
+  }
 
-    // useEffect to fetch data on component mount
-    useEffect(()=>
-    {
+  useEffect(() => {
+    fetchBook();
+  }, [bookId]);
 
-        // set loading to true
-        dispatch(setLoading({isLoading: true, loadingMsg: "Loading book data..."}))
-
-        // fetch
-        try
-        {
-            fetchBook()
-
-        }
-        catch(error)
-        {
-            // show error
-            dispatch(setInfo({shouldShow: true, infoMsg: error.message, infoType: "error"}))
-
-            // navigate
-            navigate("/")
-        }
-        finally
-        {
-            // stop loading
-            dispatch(setLoading({isLoading: false, loadingMsg: ""}))
-        }
-
-    }, [])
-
-    return (
-
-
-        <div>
-
-            {/* book details */}
-
-            <div className="flex flex-col md:flex-row bg-gray-100 p-4 rounded-lg shadow-md">
-                <div className="md:w-1/3">
-                    <h2 className="text-xl font-bold">{singleBook.bookTitle}</h2>
-                    <p className="text-gray-500">by {singleBook.bookAuthor}</p>
+  return (
+    <BackGround>
+      <div className="p-4 w-full bg-white/10 rounded-lg shadow-2xl shadow-orange-500/30 text-white">
+        <div className="grid grid-cols-5">
+          <div className="col-span-5 sm:col-span-2 xl:col-span-1 flex justify-center items-center w-full p-4">
+            <img
+              src={bookData.coverPhotoURL}
+              alt={bookData.bookTitle}
+              className="aspect-[5/8] w-60 object-cover"
+            />
+          </div>
+          <div className="col-span-5 sm:col-span-3 xl:col-span-4 p-4">
+            <h1 className="text-4xl font-bold mb-4">{bookData.bookTitle}</h1>
+            <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+              <div className="col">
+                <p className="text-xl mb-2">
+                  <strong>Author:</strong> {bookData.bookAuthor}
+                </p>
+              </div>
+              <div className="col">
+                <p className="text-lg mb-2">
+                  <strong>Page Count:</strong> {bookData.bookPageCount}
+                </p>
+              </div>
+              <div className="col">
+                <p className="text-lg mb-2">
+                  <strong>Category:</strong> {bookData.bookCategoryName}
+                </p>
+              </div>
+              <div className="col">
+                <p className="text-lg mb-2">
+                  <strong>City:</strong> {bookData.bookOwnerCity}
+                </p>
+              </div>
+              <div className="col">
+                <p className="text-lg mb-2">
+                  <strong>Uploaded On:</strong> {bookData.bookUploadDate}
+                </p>
+              </div>
+              <div className="col lg:col-span-2">
+                <div className="mt-4">
+                  <h2 className="text-2xl font-semibold mb-2">Owner</h2>
+                  <UserAvatar
+                    user={{
+                      id: bookData.bookOwnerId,
+                      name: bookData.bookOwnerName,
+                      email: bookData.bookOwnerEmail,
+                      profilePhotoURL: bookData.bookOwnerProfilePhotoURL,
+                    }}
+                  />
                 </div>
-                <div className="md:w-2/3 flex flex-wrap justify-between mt-4 md:mt-0">
-                    <div className="flex items-center mb-2 md:mb-0">
-                        <span className="text-gray-500 mr-2">Category:</span>
-                        <span className="text-gray-700">{singleBook.bookCategoryName}</span>
-                    </div>
-                    <div className="flex items-center mb-2 md:mb-0">
-                        <span className="text-gray-500 mr-2">Quantity:</span>
-                        <span className="text-gray-700">{singleBook.bookQuantity}</span>
-                    </div>
-                    <div className="flex items-center mb-2 md:mb-0">
-                        <span className="text-gray-500 mr-2">Pages:</span>
-                        <span className="text-gray-700">{singleBook.bookPageCount}</span>
-                    </div>
-                    <div className="flex items-center mb-2 md:mb-0">
-                        <span className="text-gray-500 mr-2">Owner:</span>
-                        <span className="text-gray-700">{singleBook.bookOwnerName}</span>
-                    </div>
-                </div>
+              </div>
             </div>
-
-
-            {/* all copies of the book */}
-            <div>
-
-                {singleBook.bookCopies.map((bookCopy)=>
-                (
+          </div>
+          <div className="col-span-5 flex items-start justify-center p-4">
+            {/* Book Copies Section */}
+            <div className="w-full overflow-x-auto">
+              <table className="w-full bg-gray-800 rounded-2xl border-2 border-white">
+                <thead>
+                  <tr className="hidden lg:table-row">
+                    <th className="px-4 py-2">Index</th>
+                    <th className="px-4 py-2">Borrow</th>
+                    <th className="px-4 py-2">Current Holder</th>
+                    <th className="px-4 py-2">Next Borrower</th>
+                  </tr>
+                </thead>
+                <tbody className="text-center">
+                  {bookData.bookCopies.map((copy, index) => (
                     <BookCopyRow
-                        key={bookCopy.bookCopyId}
-                        bookCopy={bookCopy}
-                    />
-                ))}
-
+                      bookCopy={copy}
+                      ownerId={bookData.bookOwnerId}
+                      fetchBook={fetchBook}
+                    ></BookCopyRow>
+                  ))}
+                </tbody>
+              </table>
             </div>
-
+          </div>
         </div>
-
-
-    )
-
-
+      </div>
+    </BackGround>
+  );
 }
 
-
-export default Book
+export default Book;
--- src/components/BookCopyRow.jsx
+++ src/components/BookCopyRow.jsx
@@ -1,59 +1,145 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import {Button} from "./"
+import { UserAvatar } from "./";
 import { bookBorrowService } from "../services";
+import { setLoading, setInfo } from "../slices";
 
-function BookCopyRow({bookCopy})
-{
- let eligible=bookCopy.requestable
- const jwt = useSelector(state=>state.auth.token)
-  async function requestHandler()
-  {
-    dispatch(setLoading({isLoading: true, loadingMsg: "request for borrow is sending."}))
-    try
-            {
-                // call the service
-                const response = await bookBorrowService.requestForBorrow(bookCopy.bookId, jwt)
+function BookCopyRow({ bookCopy, ownerId, fetchBook }) {
+  const dispatch = useDispatch();
+  const jwt = useSelector((state) => state.auth.token);
+  const userId = useSelector((state) => state.auth.authDetails.id);
+  async function requestHandler() {
+    dispatch(
+      setLoading({
+        isLoading: true,
+        loadingMsg: "request for borrow is sending.",
+      })
+    );
+    console.log("debug comment");
+    try {
+      // call the service
+      const response = await bookBorrowService.requestForBorrow(
+        bookCopy.bookId,
+        jwt
+      );
 
-                // custom status exceptions
-                if(!response.ok)
-                {
-                    throw new Error(await response.json())
-                }
+      // custom status exceptions
+      if (!response.ok) {
+        throw new Error(await response.json());
+      }
 
-                // show success message
-                dispatch(setInfo({shouldShow: true, infoMsg: await response.text(), infoType: "success"}))
+      // show success message
+      dispatch(
+        setInfo({
+          shouldShow: true,
+          infoMsg: await response.text(),
+          infoType: "success",
+        })
+      );
 
-            }
-            catch(error)
-            {
-                // set the error
-                dispatch(setInfo({shouldShow: true, infoMsg: error.message, infoType: "error"}))
-            }
-            finally
-            {
-                // stop loading
-                dispatch(setLoading({isLoading: false, loadingMsg: ""}))
+      fetchBook();
+    } catch (error) {
+      // set the error
+      dispatch(
+        setInfo({ shouldShow: true, infoMsg: error.message, infoType: "error" })
+      );
+    } finally {
+      // stop loading
+      dispatch(setLoading({ isLoading: false, loadingMsg: "" }));
 
-                // navigate
-                navigate("/")
-            }
- }
-   
-    return(
-        <>
-         <Link to = {`/bookCopies/${bookCopy.bookCopyId}`}  >
-        <div >
-          <div>
-            <li>{bookCopy.bookCopyId}</li>
-            <li>{bookCopy.bookTitle}</li>
-            <li>{bookCopy.holderName}</li>
-            <li>{bookCopy.borrowerName}</li>
-          </div>
-          {eligible && ( <Button handleClick={requestHandler}> Request for Borrow</Button>)}
-        </div>
-        </Link>
-        </>
-    )}
+      // navigate
+      navigate("/");
+    }
+  }
+
+  function getNextAvailabilityDate(dateStr) {
+    let date;
+    if (dateStr == null) {
+      date = new Date();
+    } else {
+      date = new Date(dateStr.split("-").reverse().join("-"));
+      date.setDate(date.getDate() + 30);
+    }
+
+    if (bookCopy.borrowerId != ownerId) {
+      date.setDate(date.getDate() + 30);
+    }
+    return date.toISOString().slice(0, 10).split("-").reverse().join("-");
+  }
+
+  const isAvailable =
+    bookCopy.holderId == ownerId && bookCopy.borrowerId == ownerId;
+
+  let availabilityMessage = "Available";
+  if (!isAvailable) {
+    const nextAvailableDate = getNextAvailabilityDate(
+      bookCopy.holderReceiveDate
+    );
+    availabilityMessage = `Available after: ${nextAvailableDate}`;
+  }
+
+  return (
+    <>
+      <tr key={bookCopy.bookCopyId} className="border-t border-gray-700">
+        <td className="px-4 py-2 block sm:table-cell">
+          <span className="lg:hidden text-gray-400">Id: </span>
+          {bookCopy.bookCopyId}
+        </td>
+        <td className="px-4 py-2 block sm:table-cell">
+          {bookCopy.requestable && (
+            <button
+              className="bg-green-500 hover:bg-green-600 text-white py-1 px-4 rounded-lg"
+              onClick={requestHandler}
+            >
+              Borrow
+            </button>
+          )}
+          {!bookCopy.requestable && ownerId != userId && (
+            <button className="bg-gray-500 text-white py-1 px-4 rounded-lg cursor-not-allowed opacity-30">
+              Borrow
+            </button>
+          )}
+          <p
+            className={`${
+              isAvailable ? "text-green-600" : "text-orange-500"
+            } p-2`}
+          >
+            {availabilityMessage}
+          </p>
+        </td>
+        <td className="px-4 py-2 block lg:table-cell">
+          <span className="lg:hidden text-gray-400">Current Holder: </span>
+          {bookCopy.holderId != ownerId ? (
+            <UserAvatar
+              user={{
+                id: bookCopy.holderId,
+                name: bookCopy.holderName,
+                email: bookCopy.holderEmail,
+                profilePhotoURL: bookCopy.holderProfilePhotoURL,
+              }}
+            />
+          ) : (
+            <p className="text-yellow-400">owner</p>
+          )}
+        </td>
+        <td className="px-4 py-2 block lg:table-cell">
+          <span className="lg:hidden text-gray-400">Next Borrower: </span>
+          {bookCopy.borrowerId != ownerId ? (
+            <UserAvatar
+              user={{
+                id: bookCopy.borrowerId,
+                name: bookCopy.borrowerName,
+                email: bookCopy.borrowerEmail,
+                profilePhotoURL: bookCopy.borrowerProfilePhotoURL,
+              }}
+            />
+          ) : (
+            <p className="text-yellow-400"> No one </p>
+          )}
+        </td>
+      </tr>
+    </>
+  );
+}
 
 export default BookCopyRow;
